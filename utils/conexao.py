b5b193c3fded4d5a4c9e2bc9b11ea9c30fc33b0a
Lucas Oliveira10 <lucasoliveira13125@gmail.com>
2026-06-14 18:21:04 -0300

    feat: estrutura inicial de pastas e conexão.py(HOST & PORTA_SRV)

---
+++ utils/conexao.py
@@ -0,0 +1,5 @@
+# Definindo o IP do servidor. Como o código vai rodar localmente definimos localhost para testes na própria máquina.
+HOST = '127.0.0.1' 
+
+# Definindo a porta de comunicação. Escolhi uma porta alta (acima de 1024) que está livre.
+PORTA_SRV = 5000
